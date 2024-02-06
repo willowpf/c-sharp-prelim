13b2cfe90bc5347588a77160a1203a8c29a30b4c
unknown <Admin@CSLAB236.CSSERVER.local>
2024-02-06 19:56:36 +0800

    Activity 2 ni sir

---
+++ Activity2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Activity2
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> list = new List<Car>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter new data? Y/N");
+
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    break;
+                }
+                Car car1 = new Car();
+
+                Console.WriteLine("Enter Car Brand");
+                car1.Brand = Console.ReadLine();
+
+
+                Console.WriteLine("Enter Car Color");
+                car1.Color = Console.ReadLine();
+
+
+                Console.WriteLine("Enter Car Price");
+                car1.Price = int.Parse(Console.ReadLine());
+
+                list.Add(car1);
+
+            }
+            list.ForEach(x =>
+            {
+                Console.WriteLine(x.Brand);
+                Console.WriteLine(x.Color);
+                Console.WriteLine(x.Price);
+                Console.WriteLine("=======================");
+            });
+        }
+    }
+}
